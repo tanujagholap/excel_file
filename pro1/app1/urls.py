82c681a80c3bd0fb12715a87e16add0198cb4efa
tanujagholap <tanujarbhong@gmail.com>
2024-05-17 16:16:38 +0530

    second commit

--- pro1/app1/urls.py
+++ pro1/app1/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('pro/', ProductView.as_view()),
-    path('total/', Total.as_view())
+    path('total/', TotalCalculationAPIView.as_view()),
+    path('update/<int:pk>/', ProductUpdate.as_view()),
+    path('delete/<int:pk>/', ProductDelete.as_view()),
+
 ]
--- pro1/app1/views.py
+++ pro1/app1/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import Product, ProductSerializer
 import pandas as pd
+from rest_framework.parsers import MultiPartParser
 
 
 class ProductView(APIView):
@@ -19,5 +20,36 @@ class ProductView(APIView):
             return Response(serializer.errors, status=400)
 
 
-class Total(APIView):
-    pass
+class TotalCalculationAPIView(APIView):
+    parser_classes = [MultiPartParser]
+
+    def post(self, request, format=None):
+        # if 'excel_file' not in request.FILES:
+        #     return Response({'error': 'No file uploaded'}, status=400)
+        #
+        excel_file = request.FILES['excel_file']
+
+        try:
+            df = pd.read_file(excel_file)
+            total = df['price'].sum()
+            return Response({'total': total}, status=200)
+        except Exception as e:
+            return Response({'error': str(e)}, status=400)
+
+
+class ProductUpdate(APIView):
+    def put(self, request, pk):
+        obj = Product.objects.get(id=pk)
+        serializers = ProductSerializer(data=request.data, instance=obj, partial=True)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(data=serializers.data, status=200)
+        return Response(serializers.errors, status=400)
+
+
+class ProductDelete(APIView):
+    def delete(self, request, pk):
+        obj = Product.objects.get(id=pk)
+        obj.delete()
+        return Response(data={'msg': 'deleted successfully'}, status=200)
+
